fa03c80c5d9d5524461243c6940f28263b857451
Hannah McLaughlin <hannah.mclaughlin@paconsulting.com>
2016-04-12 17:27:31 +0100

    Implemented expectation method

--- kmeans/src/lib.rs
+++ kmeans/src/lib.rs
@@ -9,12 +9,12 @@ pub struct DataPoint {
 fn read_data(file_path: &Path) -> Vec<DataPoint> {
 }*/
 
-pub fn squared_euclidean_distance(point_1: DataPoint, point_2: DataPoint) -> f64 {
+pub fn squared_euclidean_distance(point_1: &DataPoint, point_2: &DataPoint) -> f64 {
    (point_2.x - point_1.x).powi(2) + (point_2.y - point_1.y).powi(2)
 }
 
 
-pub fn get_index_of_min_val(floats: Vec<f64>) -> usize {
+pub fn get_index_of_min_val(floats: &Vec<f64>) -> usize {
 
     floats.iter()
           .enumerate()
@@ -24,21 +24,22 @@ pub fn get_index_of_min_val(floats: Vec<f64>) -> usize {
                 else { min_ind })
 }
 
-/*
 /// Assign points to clusters
 fn expectation(data: Vec<DataPoint>,
-               cluster_centroids: Vec<DataPoint>) {
+               cluster_centroids: Vec<DataPoint>) -> Vec<usize> {
 
-    let mut distance: Vec<f64>;
-    for point in data {
-        for cluster in cluster_centroids {
+    let mut distance: Vec<f64> = vec![];
+    let mut assigned_cluster: Vec<usize> = vec![];
+    for point in &data {
+        for cluster in &cluster_centroids {
             distance.push(squared_euclidean_distance(point, cluster));
         }
 
         // Index of cluster centroid that point is nearest to
-        let assignment = get_index_of_min_val(distance);
+        assigned_cluster.push(get_index_of_min_val(&distance));
     }
-}*/
+    assigned_cluster
+}
 
 /*
 /// Update cluster centres
@@ -56,7 +57,7 @@ mod tests {
         let origin = DataPoint { x: 0.0, y: 0.0};
         let point = DataPoint {x: 1.0, y: 1.0};
         let expected = 2.0;
-        let actual = squared_euclidean_distance(origin, point);
+        let actual = squared_euclidean_distance(&origin, &point);
         assert_eq!(expected, actual)
     }
 
@@ -65,15 +66,15 @@ mod tests {
         let point_1 = DataPoint { x: -999.3, y: 10.5};
         let point_2 = point_1.clone();
         let expected = 0.0;
-        let actual = squared_euclidean_distance(point_1, point_2);
+        let actual = squared_euclidean_distance(&point_1, &point_2);
         assert_eq!(expected, actual)
     }
 
     #[test]
-    fn test_dum() {
+    fn test_get_index_of_min_val() {
         let floats = vec![0.0_f64, 1.0_f64, 3.0_f64, -5.5_f64];
         let expected = 3;
-        let actual = get_index_of_min_val(floats);
+        let actual = get_index_of_min_val(&floats);
         assert_eq!(expected, actual)
     }
 }
